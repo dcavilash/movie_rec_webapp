ed9a77fe7e6572a6375a95dc57adbfbedfd63139
Avilash Barua <105745671+dcavilash@users.noreply.github.com>
2024-08-31 15:56:09 +0530

    Add files via upload

--- rec_app.py
+++ rec_app.py
@@ -25,7 +25,7 @@ def recommendme(movie, min_rating):
     rec_movie=[]
     rec_poster=[]
     rec_rating=[]
-    for i in distances[0:100]:
+    for i in distances[0:500]:
         movie_id = mv1.iloc[i[0]].id
         poster_address, rating = get_poster_and_rating(movie_id)
 
@@ -59,7 +59,7 @@ if movie_selected and movie_selected != "":
         with c21:
             st.image(m_sel_poster_address)
         with c22:
-            st.text(f"User Rating: \n {m_sel_rating}/10")
+            st.text(f"User Rating:\n{m_sel_rating}/10")
 
 if st.button("Get Recommendations"):
     rec_df = recommendme(movie_selected, min_rating)
@@ -68,11 +68,11 @@ if st.button("Get Recommendations"):
     for i in range(1, 6):  # Fill the first row
         with cols_row1[i - 1]:
             st.image(rec_df.iloc[i]['poster path'])
-            st.text(f"{rec_df.iloc[i]['title']}\n{rec_df.iloc[i]['user rating']}/10")
+            st.markdown(f"{rec_df.iloc[i]['title']}\n{rec_df.iloc[i]['user rating']}/10")
 
     st.divider()
     cols_row2 = st.columns(5)
     for i in range(6, 11):  # Fill the second row
         with cols_row2[i - 6]:
             st.image(rec_df.iloc[i]['poster path'])
-            st.text(f"{rec_df.iloc[i]['title']}\n{rec_df.iloc[i]['user rating']}/10")
+            st.markdown(f"{rec_df.iloc[i]['title']}\n{rec_df.iloc[i]['user rating']}/10")
