256b125e5a50ff4c6c024d3a9d1be2774d66a5ab
Avilash Barua <105745671+dcavilash@users.noreply.github.com>
2024-08-31 14:55:39 +0530

    Add files via upload

--- rec_app.py
+++ rec_app.py
@@ -48,6 +48,10 @@ st.divider()
 c1, c2 = st.columns(2)
 with c1:
     movie_selected = st.selectbox("Select or Search a Movie", [""] + movie_list)
+    st.divider()
+    st.write("Minimum User Rating:")
+    min_rating = float(st.slider("", min_value=0.0, max_value=8.0, step=0.1, value=0.0))
+
 if movie_selected and movie_selected != "":
     id = mv1[mv1['title'] == movie_selected]['id'].iloc[0]
     m_sel_poster_address, m_sel_rating = get_poster_and_rating(id)
@@ -57,12 +61,6 @@ if movie_selected and movie_selected != "":
             st.image(m_sel_poster_address)
         with c22:
             st.text(f"User Rating: \n {m_sel_rating}/10")
-
-col1, col2 = st.columns(2)
-with col1:
-    st.write("Minimum User Rating:")
-with col2:
-    min_rating = float(st.slider("", min_value=0.0, max_value=8.0, step=0.1, value=0.0))
 
 if st.button("Get Recommendations"):
     rec_df = recommendme(movie_selected, min_rating)
