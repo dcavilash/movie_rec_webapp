cc0802c34a5d33961c66b9bb544d2a2c401324d5
Avilash Barua <105745671+dcavilash@users.noreply.github.com>
2024-08-31 11:21:55 +0530

    Add files via upload

--- rec_app.py
+++ rec_app.py
@@ -3,63 +3,38 @@ import pickle
 import requests
 import pandas as pd
 
+# Load movie data and similarity matrix
 mv1 = pickle.load(open('movies.pkl', 'rb'))
 movie_list = mv1['title'].values
 similarity_matrix = pickle.load(open('similarity.pkl', 'rb'))
 
-
-my_api_token="eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNWY1ZTc1ZDgyNjMzYTU5NmE4ZWJkM2ZkYzgwZTM2YSIsIm5iZiI6MTcyNDk4MDY5MC4xMjY1ODQsInN1YiI6IjY2ZDExOGUwNmRmMzFjZGNhMzMwNDVkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CieuhUZpIRe6qQlEg0IlIXR28pba6V44j7GY07VkzUs"
-headers = {
-    "accept": "application/json",
-    "Authorization": "Bearer " + my_api_token
-    }
+# API authentication
+my_api_token = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkNWY1ZTc1ZDgyNjMzYTU5NmE4ZWJkM2ZkYzgwZTM2YSIsIm5iZiI6MTcyNDk4MDY5MC4xMjY1ODQsInN1YiI6IjY2ZDExOGUwNmRmMzFjZGNhMzMwNDVkZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.CieuhUZpIRe6qQlEg0IlIXR28pba6V44j7GY07VkzUs"
+headers = {"accept": "application/json", "Authorization": "Bearer " + my_api_token}
 
 def get_poster_and_rating(movie_id):
-    url="https://api.themoviedb.org/3/movie/{}?language=en-US".format(movie_id)
-    #print(url)
-    data = requests.get(url, headers=headers)
-    data = data.json()
-    poster_address = "https://image.tmdb.org/t/p/w500/" + data['poster_path']
-    rating = data['vote_average']
-    #print(data)
-    return poster_address, rating
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?language=en-US"
+    data = requests.get(url, headers=headers).json()
+    return f"https://image.tmdb.org/t/p/w500/{data['poster_path']}", data['vote_average']
 
 def recommendme(movie, min_rating):
-    index=mv1[mv1['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity_matrix[index])), reverse=True, key=lambda vector1:vector1[1])
+    index = mv1[mv1['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity_matrix[index])), reverse=True, key=lambda x: x[1])
 
     mv1_df = pd.DataFrame(mv1)
-    
-    # Filter by minimum rating
     filtered_mv1_df = mv1_df[mv1_df['vote_average'] >= min_rating - 0.5]
-    
-    # Need to add selected movie in the dataframe if its lower than min_rating entered
+
+    # Add selected movie if it's lower than min_rating
     if index not in filtered_mv1_df['id'].values:
-        row_to_add = mv1_df[mv1_df['id'] == index].iloc[0]
-        filtered_mv1_df = pd.concat([filtered_mv1_df, row_to_add.to_frame().T], ignore_index=True)
-    
-    rec_movies=[]
-    rec_posters=[]
-    rec_ratings=[]
-    for i in distance[0:15]:
-        movie_id = filtered_mv1_df.iloc[i[0]]['id']
-        poster_address, rating = get_poster_and_rating(movie_id)
-        rec_movies.append(filtered_mv1_df.iloc[i[0]]['title'])
-        rec_posters.append(poster_address)
-        rec_ratings.append(rating)
-    rec_df = pd.DataFrame({
-        'title': rec_movies,
-        'poster path': rec_posters,
-        'user rating': rec_ratings
-    })
-    #filtering with live min_rating again
-    rec_df = rec_df[rec_df['user rating'] >= min_rating]
-    
-    return rec_df
+        filtered_mv1_df = pd.concat([filtered_mv1_df, mv1_df[mv1_df['id'] == index].iloc[0].to_frame().T], ignore_index=True)
 
-####################        Page design      ########################################################################################################
+    rec_df = filtered_mv1_df.iloc[[i[0] for i in distances[:18]]]
+    rec_df['poster path'] = rec_df['id'].apply(lambda x: get_poster_and_rating(x)[0])
+    rec_df['user rating'] = rec_df['id'].apply(lambda x: get_poster_and_rating(x)[1])
 
-st.title("Movie Recommendation System With Current User Rating filter")
+    return rec_df[rec_df['user rating'] >= min_rating]
+
+st.title("Movie Recommendation System With Current User Rating Filter")
 st.header("by Avilash Barua")
 st.divider()
 
@@ -67,40 +42,27 @@ movie_selected = st.selectbox("Select or Search a Movie", movie_list)
 
 col1, col2 = st.columns(2)
 with col1:
-    st.write("Minimun User Rating:")
+    st.write("Minimum User Rating:")
 with col2:
-    min_rating = float(st.slider("", min_value = 0.0, max_value = 8.0, step = 0.1))
-
+    min_rating = float(st.slider("", min_value=0.0, max_value=8.0, step=0.1, value=0.0))
 
 if st.button("Get Recommendations"):
     rec_df = recommendme(movie_selected, min_rating)
-    
+
     cols_row0 = st.columns(4)
     with cols_row0[1]:
         st.image(rec_df.iloc[0]['poster path'])
-        #
-        #
     with cols_row0[2]:
         st.text(rec_df.iloc[0]['title'])
         st.divider()
-        st.text("User Rating: ")
-        st.text("{}/10".format(rec_df.iloc[0]['user rating']))
+        st.text(f"User Rating: {rec_df.iloc[0]['user rating']}/10")
 
-
-    # Display the next 10 movies in two rows of 5
     st.text("You might like:")
-    
-    cols_row1 = st.columns(5)
-    cols_row2 = st.columns(5)
-    
-    for i in range(1, 6):
-        with cols_row1[i - 1]:
+
+    cols_row1, cols_row2 = st.columns(5, 5)
+    for i in range(1, 11):
+        col = cols_row1 if i <= 5 else cols_row2
+        with col[i % 5]:
             st.image(rec_df.iloc[i]['poster path'])
             st.text(rec_df.iloc[i]['title'])
-            st.text("{}/10".format(rec_df.iloc[i]['user rating']))
-    
-    for i in range(6, 11):
-        with cols_row2[i - 6]:
-            st.image(rec_df.iloc[i]['poster path'])
-            st.text(rec_df.iloc[i]['title'])
-            st.text("{}/10".format(rec_df.iloc[i]['user rating']))
+            st.text(f"User Rating: {rec_df.iloc[i]['user rating']}/10")
